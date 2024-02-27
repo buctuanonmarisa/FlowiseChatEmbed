7c0a78cbb23a875809e392b829599cdd77ebcf67
buctuanonmarisa <buctuanonmarisa@gmail.com>
2024-02-27 13:13:34 +0800

    fix y overflow chat

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -432,9 +432,9 @@ export const Bot = (botProps: BotProps & { class?: string }) => {
       >
         <div class="flex w-full h-full justify-center">
           <div
-            style={{ 'padding-bottom': '100px', 'padding-top': '70px' }}
+            style={{ 'padding-bottom': '100px', 'padding-top': '70px', 'height':' 600px' }}
             ref={chatContainer}
-            class="overflow-y-scroll min-w-full w-full min-h-full px-3 pt-10 relative scrollable-container chatbot-chat-view scroll-smooth"
+            class="messagelist"
           >
             <For each={[...messages()]}>
               {(message, index) => (
